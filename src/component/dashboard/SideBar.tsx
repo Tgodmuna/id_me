ecc4d81494510a575b8a548497ec7e4a1ea09eee
ThankGod <aguthankgod@gmail.com>
2024-06-08 10:19:07 +0100

    Feat : new component for sidebar rendering

---
+++ src/component/dashboard/SideBar.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import { FaHome, FaUser, FaSignOutAlt, FaBell } from "react-icons/fa";
+import Logo from "../Logo";
+
+const Sidebar: React.FC = () => {
+	const location = useLocation();
+
+	return (
+		<aside className='bg-gray-800 text-gray-100 h-screen mr-3 fixed w-[20rem] flex flex-col justify-between shadow-xl shadow-black '>
+			<div className='p-4 flex flex-col items-start gap-[4rem] h-[57rem]'>
+				<h2 className='text-2xl font-bold'>Dashboard</h2>
+				<ul className='mt-4 flex flex-col gap-[2rem] bg- w-full h-[45rem]'>
+					<li
+						className={`flex items-center mb-4 hover:bg-blue-300 p-2 rounded-lg transition-colors duration-300 ${
+							location.pathname === "/dashboard"
+								? "text-neutral-600 p-2 underline  bg-slate-50"
+								: ""
+						}`}
+					>
+						<FaHome className='mr-2' />
+						<Link to='/dashboard'>Home</Link>
+					</li>
+					<li
+						className={`flex items-center mb-4 hover:bg-blue-300 p-2 rounded-lg transition-colors duration-300 ${
+							location.pathname === "/dashboard/profile"
+								? "text-neutral-600 p-2 underline  bg-slate-50"
+								: ""
+						}`}
+					>
+						<FaUser className='mr-2' />
+						<Link to='/dashboard/profile'>Profile</Link>
+					</li>
+
+					<li
+						className={`flex items-center mb-4 hover:bg-blue-300 p-2 rounded-lg transition-colors duration-300 ${
+							location.pathname === "/logout"
+								? "text-neutral-600 p-2 underline  bg-slate-50"
+								: ""
+						}`}
+					>
+						<FaSignOutAlt className='mr-2' />
+						<Link to='/logout'>Logout</Link>
+					</li>
+					<li
+						className={`flex items-center mb-4 hover:bg-blue-300 p-2 rounded-lg transition-colors duration-300 ${
+							location.pathname === "/dashboard/notification"
+								? "text-neutral-600 p-2 underline  bg-slate-50"
+								: ""
+						}`}
+					>
+						<FaBell className='mr-2' />
+						<Link to='/dashboard/notification'>Notification</Link>
+					</li>
+				</ul>
+			</div>
+			<div className='p-4'>
+				<Logo />
+			</div>
+		</aside>
+	);
+};
+
+export default Sidebar;
