92640353aa859286ee3cef09fe6076c2ed98bf06
ThankGod <aguthankgod@gmail.com>
2024-06-12 20:13:20 +0100

    make home page responsive

--- src/component/INDEX.tsx
+++ src/component/INDEX.tsx
@@ -4,9 +4,7 @@ import Logo from "./Logo";
 
 const INDEX = () => {
 	return (
-		<div
-			about={"Landing page for the Verificationboard app."}
-		>
+		<div about={"Landing page for the Verificationboard app."}>
 			<Header />
 			<LandingPage />
 			<Footer />
@@ -18,26 +16,24 @@ export default INDEX;
 
 const Header: React.FC = () => {
 	return (
-		<header className='bg-gray-800 text-white p-4 shadow-md'>
-			<nav className='container mx-auto flex justify-between items-center'>
-				<Link to='/' className='flex items-center'>
-					<Logo />
+		<header className='bg-gray-800  mx-auto flex justify-between items-center  text-white p-4 shadow-md w-full max-w-full'>
+			<Link to='/' className='flex items-center bg'>
+				<Logo />
+			</Link>
+			<div>
+				<Link to='/' className='mr-4 hover:underline'>
+					Home
 				</Link>
-				<div>
-					<Link to='/' className='mr-4 hover:underline'>
-						Home
-					</Link>
-					<Link to='/about' className='mr-4 hover:underline'>
-						About
-					</Link>
-					<Link to='/login' className='mr-4 hover:underline'>
-						Login
-					</Link>
-					<Link to='/register' className='hover:underline'>
-						Sign Up
-					</Link>
-				</div>
-			</nav>
+				<Link to='/about' className='mr-4 hover:underline'>
+					About
+				</Link>
+				<Link to='/login' className='mr-4 hover:underline'>
+					Login
+				</Link>
+				<Link to='/register' className='hover:underline'>
+					Sign Up
+				</Link>
+			</div>
 		</header>
 	);
 };
@@ -56,10 +52,10 @@ const LandingPage: React.FC = () => {
 	return (
 		<div className='flex flex-col items-center justify-center min-h-screen bg-light text-dark'>
 			<div className='max-w-2xl text-center p-6 bg-gray-800 shadow-lg rounded-lg'>
-				<h1 className='text-5xl font-bold text-white mb-6 dancing-script '>
+				<h1 className='md:text-5xl text-3xl font-bold text-white mb-6 dancing-script '>
 					Welcome to Verificationboard
 				</h1>
-				<p className='text-lg text-gray-200 mb-6'>
+				<p className='md:text-lg text-gray-200 mb-6'>
 					Experience a seamless and secure way to manage user data with our platform. Sign up today
 					to get started or log in to access your account.
 				</p>
--- src/component/Logo.tsx
+++ src/component/Logo.tsx
@@ -3,10 +3,10 @@ import { IoShieldCheckmark } from "react-icons/io5";
 
 const Logo: React.FC = () => {
 	return (
-		<div className='flex items-center'>
-			<IoShieldCheckmark className='text-white text-5xl mr-2' />
+		<div className='flex items-center jus'>
+			<IoShieldCheckmark className='text-white text-3xl md:text-5xl mr-2' />
 			<div className={`flex flex-col justify-center`}>
-				<h1 className='text-2xl orbitron font-bold'>Verificationboard</h1>
+				<h1 className='md:text-2xl orbitron md:font-bold'>Verificationboard</h1>
 				<p className='text-sm text-neutral-600'>Trust and Verification</p>
 			</div>
 		</div>
