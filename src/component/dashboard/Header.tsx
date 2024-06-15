0f4b373f7ae52b8bc9f14d30f2014c5c8f9994c9
ThankGod <aguthankgod@gmail.com>
2024-06-15 02:31:01 +0100

    responsive fix 3rd

--- src/component/dashboard/Header.tsx
+++ src/component/dashboard/Header.tsx
@@ -7,7 +7,9 @@ const Header: React.FC<{ ToggleSidebar: () => void }> = ({ ToggleSidebar }) => {
 		<header className='bg-inherit shadow-gray-500 rounded-xl p-4 shadow-md'>
 			<div className='container mx-auto flex justify-between items-center'>
 				<FaBars onClick={() => ToggleSidebar()} className={`text-3xl md:hidden block text-gray-800`} />
-				<h1 className=' text-4xl orbitron text-gray-800 font-bold'>Dashboard</h1>
+				<Link to={"/dashboard"} className=' text-4xl orbitron text-gray-800 font-bold'>
+					Dashboard
+				</Link>
 				<nav>
 					<ul className='flex space-x-4 text-white'>
 						<li>
